2091e8d8f2e9f85a52698218ecb399fd8402fac0
Alvaro Chirinos <65077832+alvaro-chiri@users.noreply.github.com>
2023-10-25 21:48:48 -0400

    Time: 37 ms (59.13%) | Memory: 16 MB (94.49%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,10 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1 or n == 0:
+        if n == 1:
             return 1
-        
-        arr = [1,1]
-        for i in range(2, n+1):
-            temp = arr[i-1] + arr[i-2]
-            arr.append(temp)
-        return arr[n]
+
+        one_before = 1
+        two_before = 1
+        total = 0
+
+        for i in range(n-1):
+            total = one_before + two_before
+            two_before = one_before
+            one_before = total
+
+        return total
